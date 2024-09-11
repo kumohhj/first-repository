fd7b46b0bc085747cbbfa1eb89dca82acaef24b7
parkheejun-kumoh <dmarkaql@gmail.com>
2024-09-11 15:20:40 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,5 @@
 def print_hello():
     print("Hello World!!")
 
-def print_KIT():
-    print("Kumoh National Institute or Techonlogy.")
-
 if __name__ == '__main__':
-    print_KIT()
     print_hello()
